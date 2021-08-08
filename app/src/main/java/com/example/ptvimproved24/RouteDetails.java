29db288f54204fff72121f3442cda368578ec104
DanistyWuKids(づ｡◕‿‿◕｡)づ <2867731-DanistyWuKids@users.noreply.gitlab.com>
2021-08-08 19:25:00 +1000

    Adding route details page

--- app/src/main/java/com/example/ptvimproved24/RouteDetails.java
+++ app/src/main/java/com/example/ptvimproved24/RouteDetails.java
@@ -1,2 +1,19 @@
-package com.example.ptvimproved24;public class RouteDetails {
+package com.example.ptvimproved24;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RouteDetails extends AppCompatActivity {
+    // Used for saved routes, showing both direction
+
+    // Passedin Route_id
+
+    private static final String TAG = "RouteDetails";
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routedetails);
+    }
+
 }
