1692ab27131f5080caf74cbe77ab1503e81ae5b2
MutianL <mutianl1@student.unimelb.edu.au>
2021-09-12 19:12:09 +0800

    parse json to object for disruption class

--- app/src/main/java/com/example/ptvimproved24/Fragment_Disruptions.java
+++ app/src/main/java/com/example/ptvimproved24/Fragment_Disruptions.java
@@ -10,20 +10,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.ptvimproved24.databinding.FragmentHomeBinding;
-import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import okhttp3.ResponseBody;
 
 
 public class Fragment_Disruptions extends Fragment {
@@ -42,7 +41,7 @@ public class Fragment_Disruptions extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        getDistruptionInfo();
+        getDisruptionInfo();
     }
 
     @Override
@@ -50,7 +49,28 @@ public class Fragment_Disruptions extends Fragment {
         super.onDestroyView();
     }
 
-    public void getDistruptionInfo(){
+    private ArrayList<Disruptions> getArrayListFromJsonArray(JSONArray jsonArray) throws JSONException {
+        ArrayList<Disruptions> result = new ArrayList<>();
+        for(int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            int id = jsonObject.getInt("disruption_id");
+            String title = jsonObject.getString("title");
+            String details = jsonObject.getString("description");
+            String status = jsonObject.getString("disruption_status");
+            String dateTime = jsonObject.getString("from_date");
+            String reflink = jsonObject.getString("url");
+            String type = jsonObject.getString("disruption_type");
+            //TODO: create mapping between type String and int
+            Disruptions d = new Disruptions(id, title, dateTime, 1);
+            d.setDetails(details);
+            d.setStatus(status);
+            d.setReflink(reflink);
+            result.add(d);
+        }
+        return result;
+    }
+
+    public void getDisruptionInfo(){
         try {
             System.out.println(commonDataRequest.disruptions());
             String url = commonDataRequest.disruptions();
@@ -65,15 +85,56 @@ public class Fragment_Disruptions extends Fragment {
                 @Override
                 public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                     if (response.isSuccessful()){
-                        System.out.println(response.body().string());
+                        String responseBody = response.body().string();
                         try {
-                            JSONObject alldisruptions = new JSONObject(builder.toString());
-//                            JSONArray array = alldisruptions.getJSONArray();
+                            JSONObject jsonObj = new JSONObject(responseBody);
+                            JSONObject allDisruption = jsonObj.getJSONObject("disruptions");
+                            JSONArray general = allDisruption.getJSONArray("general");
+                            JSONArray metroTrain = allDisruption.getJSONArray("metro_train");
+                            JSONArray metroTram = allDisruption.getJSONArray("metro_tram");
+                            JSONArray metroBus = allDisruption.getJSONArray("metro_bus");
+                            JSONArray regionalTrain = allDisruption.getJSONArray("regional_train");
+                            JSONArray regionalCoach = allDisruption.getJSONArray("regional_coach");
+                            JSONArray regionalBus = allDisruption.getJSONArray("regional_bus");
+                            JSONArray schoolBus = allDisruption.getJSONArray("school_bus");
+                            JSONArray telebus = allDisruption.getJSONArray("telebus");
+                            JSONArray nightBus = allDisruption.getJSONArray("night_bus");
+                            JSONArray ferry = allDisruption.getJSONArray("ferry");
+                            JSONArray interstateTrain = allDisruption.getJSONArray("interstate_train");
+                            JSONArray skybus = allDisruption.getJSONArray("skybus");
+                            JSONArray taxi = allDisruption.getJSONArray("taxi");
 
+                            ArrayList<Disruptions> generalArray = getArrayListFromJsonArray(general);
+                            ArrayList<Disruptions> metroTrainArray = getArrayListFromJsonArray(metroTrain);
+                            ArrayList<Disruptions> metroTramArray = getArrayListFromJsonArray(metroTram);
+                            ArrayList<Disruptions> metroBusArray = getArrayListFromJsonArray(metroBus);
+                            ArrayList<Disruptions> regionalTrainArray = getArrayListFromJsonArray(regionalTrain);
+                            ArrayList<Disruptions> regionalCoachArray = getArrayListFromJsonArray(regionalCoach);
+                            ArrayList<Disruptions> regionalBusArray = getArrayListFromJsonArray(regionalBus);
+                            ArrayList<Disruptions> schoolBusArray = getArrayListFromJsonArray(schoolBus);
+                            ArrayList<Disruptions> telebusArray = getArrayListFromJsonArray(telebus);
+                            ArrayList<Disruptions> nightBusArray = getArrayListFromJsonArray(nightBus);
+                            ArrayList<Disruptions> ferryArray = getArrayListFromJsonArray(ferry);
+                            ArrayList<Disruptions> interstateTrainArray = getArrayListFromJsonArray(interstateTrain);
+                            ArrayList<Disruptions> skybusArray = getArrayListFromJsonArray(skybus);
+                            ArrayList<Disruptions> taxiArray = getArrayListFromJsonArray(taxi);
 
+//                            System.out.println(generalArray.size());
+//                            System.out.println(metroTrainArray.size());
+//                            System.out.println(metroTramArray.size());
+//                            System.out.println(metroBusArray.size());
+//                            System.out.println(regionalTrainArray.size());
+//                            System.out.println(regionalCoachArray.size());
+//                            System.out.println(regionalBusArray.size());
+//                            System.out.println(schoolBusArray.size());
+//                            System.out.println(telebusArray.size());
+//                            System.out.println(nightBusArray.size());
+//                            System.out.println(ferryArray.size());
+//                            System.out.println(interstateTrainArray.size());
+//                            System.out.println(skybusArray.size());
+//                            System.out.println(taxiArray.size());
 
-
-
+                            //TODO: add the data to UI
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
