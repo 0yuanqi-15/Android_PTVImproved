2445f848e22959ed6710481bf5ec227c7388cb9b
DanistyWuKids(づ｡◕‿‿◕｡)づ <2867731-DanistyWuKids@users.noreply.gitlab.com>
2021-09-26 00:08:53 +1000

    Adding Stops info & stops List adapters

---
+++ app/src/main/java/com/example/ptvimproved24/stopsListAdapter.java
@@ -0,0 +1,27 @@
+package com.example.ptvimproved24;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class stopsListAdapter extends ArrayAdapter<stops> {
+    private Context mContext;
+    int mResource;
+    public stopsListAdapter(@NonNull Context context, int resource, @NonNull ArrayList<stops> objects) {
+        super(context, resource, objects);
+        mContext =context;
+        mResource = resource;
+    }
+
+//    @NonNull
+//    @Override
+//    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//      // Ref as SavedStopListAdapter to continue
+//    }
+}
