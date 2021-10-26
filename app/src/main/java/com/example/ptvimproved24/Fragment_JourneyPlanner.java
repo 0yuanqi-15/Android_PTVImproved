f1fc5fc7a9879648f2192d29173af55fa9b69650
DanistyWuKids(づ｡◕‿‿◕｡)づ <2867731-DanistyWuKids@users.noreply.gitlab.com>
2021-10-26 23:55:04 +1100

    Adding gecko for testing webview

--- app/src/main/java/com/example/ptvimproved24/Fragment_JourneyPlanner.java
+++ app/src/main/java/com/example/ptvimproved24/Fragment_JourneyPlanner.java
@@ -1,5 +1,6 @@
 package com.example.ptvimproved24;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,36 +13,46 @@ import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+//
+//import org.mozilla.geckoview.GeckoRuntime;
+//import org.mozilla.geckoview.GeckoSession;
+//import org.mozilla.geckoview.GeckoView;
 
 public class Fragment_JourneyPlanner extends Fragment {
+    private static final String URL = "https://www.ptv.vic.gov.au/journey";
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_journey_planner, container, false);
-    }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        final String URL = "https://www.ptv.vic.gov.au/journey";
-        super.onViewCreated(view, savedInstanceState);
-
-        WebView mWebView = (WebView) view.findViewById(R.id.frag_journey_webview);
+        View v= inflater.inflate(R.layout.fragment_journey_planner, container, false);
+        WebView mWebView = (WebView) v.findViewById(R.id.frag_journey_webview);
+        mWebView.getSettings().setUserAgentString("");
         WebSettings webSettings = mWebView.getSettings();
-        webSettings.setLoadWithOverviewMode(true);
         webSettings.setJavaScriptEnabled(true);
         webSettings.setGeolocationEnabled(true);
         webSettings.setUseWideViewPort(true);
         webSettings.setLoadsImagesAutomatically(true);
-        webSettings.setAllowContentAccess(true);
-        webSettings.setAllowFileAccess(true);
-        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
         webSettings.setDefaultTextEncodingName("utf-8");
 
         mWebView.loadUrl(URL);
         mWebView.setWebViewClient(new WebViewClient());
+
+        return v;
     }
+
+
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+////        return super.onCreateView(inflater, container, savedInstanceState);
+//        View v= inflater.inflate(R.layout.fragment_journey_planner, container, false);
+//
+//
+//        return v;
+//    }
 
     @Override
     public void onDestroyView() {
