a6d12f902ab9a4fd651e64c9bd78902064385915
DanistyWuKids(づ｡◕‿‿◕｡)づ <2867731-DanistyWuKids@users.noreply.gitlab.com>
2021-10-27 14:43:14 +1100

    Update journey planner with sysbrowser due to website js fault

--- app/src/main/java/com/example/ptvimproved24/Fragment_JourneyPlanner.java
+++ app/src/main/java/com/example/ptvimproved24/Fragment_JourneyPlanner.java
@@ -1,6 +1,8 @@
 package com.example.ptvimproved24;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 //
 //import org.mozilla.geckoview.GeckoRuntime;
 //import org.mozilla.geckoview.GeckoSession;
@@ -28,17 +31,32 @@ public class Fragment_JourneyPlanner extends Fragment {
         // Inflate the layout for this fragment
 
         View v= inflater.inflate(R.layout.fragment_journey_planner, container, false);
-        WebView mWebView = (WebView) v.findViewById(R.id.frag_journey_webview);
-        mWebView.getSettings().setUserAgentString("");
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setGeolocationEnabled(true);
-        webSettings.setUseWideViewPort(true);
-        webSettings.setLoadsImagesAutomatically(true);
-        webSettings.setDefaultTextEncodingName("utf-8");
+//        WebView mWebView = (WebView) v.findViewById(R.id.frag_journey_webview);
+//        mWebView.getSettings().setUserAgentString("");
+//        WebSettings webSettings = mWebView.getSettings();
+//        webSettings.setJavaScriptEnabled(true);
+//        webSettings.setGeolocationEnabled(true);
+//        webSettings.setUseWideViewPort(true);
+//        webSettings.setLoadsImagesAutomatically(true);
+//        webSettings.setDefaultTextEncodingName("utf-8");
+//
+//        mWebView.loadUrl(URL);
+//        mWebView.setWebViewClient(new WebViewClient());
 
-        mWebView.loadUrl(URL);
-        mWebView.setWebViewClient(new WebViewClient());
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(URL));
+        startActivity(i);
+
+
+        Button button = (Button) v.findViewById( R.id.fragbutton_journeyplanner);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(URL));
+                startActivity(i);
+            }
+        });
 
         return v;
     }
@@ -53,9 +71,4 @@ public class Fragment_JourneyPlanner extends Fragment {
 //
 //        return v;
 //    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-    }
 }
