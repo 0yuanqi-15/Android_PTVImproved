b47fbd586caf7b236ba48b657df6c456e60327e0
yishengwang-comp90041 <70135464+yishengwang-comp90041@users.noreply.github.com>
2021-10-28 16:39:55 +0800

    update stop detail view

--- app/src/main/java/com/example/ptvimproved24/StopDetailAdapter.java
+++ app/src/main/java/com/example/ptvimproved24/StopDetailAdapter.java
@@ -1,10 +1,12 @@
 package com.example.ptvimproved24;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -28,15 +30,36 @@ public class StopDetailAdapter extends ArrayAdapter<Route> {
         String stopTime = getItem(position).getScheduleDepart();
         String stopDirection = getItem(position).getDestination_name();
 
+        int routeType = getItem(position).getRoute_type();
+
+
+
         LayoutInflater inflater = LayoutInflater.from(mContext);
         convertView = inflater.inflate(mResource, parent, false);
 
         TextView direction = (TextView) convertView.findViewById(R.id.stop_direction);
         TextView route = (TextView) convertView.findViewById(R.id.stop_route);
         TextView time = (TextView) convertView.findViewById(R.id.stop_time);
+        ImageView image = (ImageView) convertView.findViewById(R.id.stop_time_image);
 
+        if (routeType == 1) {
+            route.setBackgroundColor(Color.YELLOW);
+            image.setBackgroundColor(Color.YELLOW);
+        }
+        else if (routeType == 2) {
+            route.setBackgroundColor(Color.GREEN);
+            image.setBackgroundColor(Color.GREEN);
+        }
+        else if (routeType == 3) {
+            route.setBackgroundColor(Color.RED);
+            image.setBackgroundColor(Color.RED);
+        }
+        else if (routeType == 4) {
+            route.setBackgroundColor(Color.BLUE);
+            image.setBackgroundColor(Color.BLUE);
+        }
 
-        direction.setText(stopDirection);
+        direction.setText("To: " + stopDirection);
         route.setText(stopRoute);
         time.setText(stopTime);
 
